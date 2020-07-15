ecd785388240e6bddfffdd79636f7caebf0384f8
harucanger <58492265+harucanger@users.noreply.github.com>
2020-07-15 18:50:01 +0900

    initial commit
    iSara practice

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>iSara[イサラ]|バンコクのノマドエンジニア育成講座</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/https://unpkg.com/ress/dist/ress.min.css">
+    <link href="<?php echo get_template_directory_uri(); ?>/https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
+<?php wp_head(); ?>
+</head>
+<body>
+      <!-- header -->
+    <header class="fixed-top bg-white">
+      <div class="container mx-auto col-12 col-md-8">
+        <div class="main-logo float-left pt-4"><img class=" align-middle" src='<?php echo get_template_directory_uri(); ?>/Users/user/Dropbox/My Mac (yuzanoMacBook-Air.local)/Documents/iSaraWp/iSaraPics/isaralogo.png" alt="iSara logo'></div>
+        <div class="main-text float-left mt-5">バンコクのノマドエンジニア育成講座</div>
+        <div class="start-btn float-right "><a class="btn rounded-pill py-3 text-white" href="#" role="button">
+            お問い合わせ / 資料請求はこちら</a>
+        </div>
+      </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php get_header();?>
+    <!-- TOP wrapper -->
+    <div class="top-wrapper col-12 mx-auto">
+      <div class="main text-center">
+      <div class="main-text pt-4"><h2 class="mb-">プログラミングで<br>人生の安定を手に入れよう</h2></div>
+      <div class="main-image pt-3"><img src="<?php echo get_template_directory_uri(); ?>/Users/user/Dropbox/My Mac (yuzanoMacBook-Air.local)/Documents/iSaraWp/iSaraPics/isaralogo.png" alt=""></div>
+      <div class="sub-name "><p>バンコクのノマドエンジニア育成講座<br>iSara[イサラ]</p></div>
+      </div>
+    </div>
+
+    <!-- Start wrapper -->
+    <div class="start-wrapper col-12 text-center">
+      <p class="start-message text-white font-weight-bold">まずは２０日間で<br>
+      月１０万円稼げるスキルを手に入れよう。</p>
+      <div class="sub-message"><p class="explain d-block rounded">※受講料金は実質０円です。詳しくは資料請求をどうぞ。</p></div>
+      <div class="start-btn start-btn-big"><a class="btn rounded-pill text-white font-weight-bold" href="#" role="button">
+          お問い合わせ&資料請求はこちら</a>
+      </div>
+        <p class="recruit-message text-white ">第６期生：2019年11月25日 ~ 2019年12月14日＊締め切りました</p>
+        <p class="recruit-message text-white ">第７期生：2020年2月17日 ~ 2019年3月7日＊締め切りました</p>
+        <p class="recruit-message message3 text-white ">第8期生：2020年4月〜5月ごろを予定＊資料請求受付中です</p>
+        <div class="links row d-flex justify-content-center pt-2 pb">
+          <div class="twitter mr-3"><a class="btn text-white font-weight-bold" href="#" role="button"><i class="fab fa-twitter"></i> ツイート</a></div>
+          <div class="facebook mr-3"><a class="btn btn-primary text-white font-weight-bold" href="#" role="button"><i class="fas fa-thumbs-up"></i> いいね！</a></div>
+          <div class="share mr-3"><a class="btn btn-primary text-white font-weight-bold" href="#" role="button">シェア</a></div>
+        </div>
+    </div>
+
+    <!-- Message wrapper -->
+    <div class="message-wrapper text-center mx-auto" >
+      <div class="top-message d-inline-block">
+          <p class="frame-box-001">エンジニアとして本当の自由を手に入れるためには？</p>
+      </div>
+      <div class="main-messages">
+        <p class="message-1">エンジニア需要の高まりに伴い、プログラミングスクールが増えています。<br>
+            しかしそこでの学習の先は、 提携して決められた就職先に就職すること。</p>
+        <p class="message-2">これで本当にいいのですか？<br>
+            日本人エンジニアはアメリカと較べて給料が格段に低い。<br>
+            その理由がここにあり、 エンジニアは自分の給料をコントロールすべきなのです。</p>
+        <p class="message-3">スキルを身につけたエンジニアは、人生をコントロールすることでより自由に。<br>
+            そこで必要なのが <span class="kasegu font-weight-bold" >「稼ぐ力」</span>です。</p>
+      </div>
+      <div class="about text-center rounded-circle mx-auto"><p>ABOUT</p></div>
+    </div>
+    
+    
+    <?php wp_footer(); ?>
+</body>
+</html>
